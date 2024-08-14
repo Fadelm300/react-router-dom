4342978396087cc7bc94919828f6bd598b6a0289
Fadem300 <fadel.m200@gmail.com>
2024-08-14 20:14:30 +0300

    c

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
-// src/App.jsx
+import { useState } from 'react';
+import NavBar from './components/NavBar';
+import { Route, Routes } from 'react-router-dom';
+import MailboxForm from './components/MailboxForm';
+import MailboxList from './components/MailboxList';
+import MailboxDetails from './components/MailboxDetails';
+import './App.css';
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [mailboxes, setMailboxes] = useState([]);
+
+  const addBox = (newMailbox) => {
+    newMailbox._id = mailboxes.length + 1;
+    setMailboxes([...mailboxes, newMailbox]);
+  };
+
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<main><h1>Post Office</h1></main>} />
+        <Route path="mailboxes" element={<MailboxList mailboxes={mailboxes} />} />
+        <Route path="mailboxes/:mailboxId" element={<MailboxDetails mailboxes={mailboxes} />} />
+        <Route path="new-mailbox" element={<MailboxForm addBox={addBox} />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
